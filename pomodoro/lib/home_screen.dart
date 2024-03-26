e7fd2c8bcadbf0f33f7fe1b57b66d0f935b47864
Dongjin <dongqui9@gmail.com>
2024-03-26 18:31:33 +0900

    pause and stop

--- pomodoro/lib/home_screen.dart
+++ pomodoro/lib/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -21,6 +22,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onStartPressed() {
     timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -46,10 +57,14 @@ class _HomeScreenState extends State<HomeScreen> {
                 flex: 4,
                 child: Center(
                   child: IconButton(
-                    onPressed: onStartPressed,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
                     iconSize: 99,
                     color: Theme.of(context).cardColor,
-                    icon: const Icon(Icons.play_circle_outlined),
+                    icon: Icon(
+                      isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_outline,
+                    ),
                   ),
                 )),
             Flexible(
