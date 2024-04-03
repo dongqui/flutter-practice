bf935e5f870264ffea2173307ea7dd3646b1b7b6
Dongjin <dongqui9@gmail.com>
2024-04-03 16:42:55 +0900

    feat: detail page

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:toonflix/widgets/webtoon.dart';
 import '../models/webtoon.dart';
 import 'package:toonflix/servcies/api_servcie.dart';
 
@@ -52,37 +53,8 @@ class HomeScreen extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
         final webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: const Color(0x80000000).withOpacity(0.5),
-                      offset: const Offset(10, 10),
-                      blurRadius: 15,
-                    )
-                  ]),
-              child: Image.network(
-                webtoon.thumb,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-            )
-          ],
-        );
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
       },
       itemCount: snapshot.data!.length,
       separatorBuilder: (context, index) => const SizedBox(
